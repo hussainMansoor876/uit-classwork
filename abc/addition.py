1d2ea234e6d0d63c0e604ddd4d12fadf6440debf
Mansoor Hussain <hussainmansoor876@gmail.com>
2020-03-12 21:13:47 +0500

    init

---
+++ abc/addition.py
@@ -0,0 +1,15 @@
+class Calculation():
+    def __init__(self):
+        super().__init__()
+
+    def add(self, a, b):
+        return a + b
+
+    def mul(self, a, b):
+        return a * b
+    
+    def div(self, a, b):
+        return a / b
+    
+    def sub(self, a, b):
+        return a - b
---
+++ app.py
@@ -0,0 +1,7 @@
+# from addition import Calculation
+
+# print(Calculation().add(6,7))
+# print(Calculation().mul(6,9))
+# print(Calculation().sub(6,7))
+# print(Calculation().div(6,7))
+
